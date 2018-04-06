c06c52850963531f7aea813ad39c06ec661cbc23
Werner Hillenbrand <1767801@cbw.intern>
2018-04-06 14:41:50 +0200

    labyrinth(cbw) labyrinth-server

---
+++ src/labyrinth_server/LabyrinthClientSwing.java
@@ -0,0 +1,80 @@
+package labyrinth.client;
+
+import labyrinth.protocol.LabyrinthService;
+import labyrinth.protocol.PlayerService;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
+public class LabyrinthClientSwing extends JFrame implements ActionListener, KeyListener {
+
+    private final JButton button;
+
+    private PlayerService player;
+
+    public LabyrinthClientSwing() {
+        setLayout(new FlowLayout());
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        button = new JButton("Klick mich!");
+        button.addActionListener(this);
+        add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        try {
+            LabyrinthService labyrinth = (LabyrinthService) Naming.lookup(LabyrinthService.NAME);
+            player = labyrinth.login();
+            button.removeKeyListener(this);
+            button.addKeyListener(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent event) {
+        // empty
+    }
+
+    @Override
+    public void keyPressed(KeyEvent event) {
+        try {
+            switch (event.getKeyCode()) {
+                case KeyEvent.VK_S:
+                    player.shoot();
+                    break;
+                case KeyEvent.VK_UP:
+                    player.moveForward();
+                    break;
+                case KeyEvent.VK_LEFT:
+                    player.turnLeft();
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    player.turnRight();
+                    break;
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent event) {
+        // empty
+    }
+
+    public static void main(String[] args) throws Exception {
+        new LabyrinthClientSwing().setVisible(true);
+    }
+
+}
---
+++ src/labyrinth_server/protocol/PlayerService.java
@@ -0,0 +1,19 @@
+package labyrinth.protocol;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Created by sreinck on 09.11.16.
+ */
+public interface PlayerService extends Remote {
+
+    String moveForward() throws RemoteException;
+
+    String turnLeft() throws RemoteException;
+
+    String turnRight() throws RemoteException;
+
+    String shoot() throws RemoteException;
+
+}
